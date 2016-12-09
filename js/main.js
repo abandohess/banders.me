64d9dde858a49847afe1dcda7853e2dd2fe655da
abandohess <bandersman@gmail.com>
2016-12-09 04:17:50 -0500

    scrolling

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-// $('document').ready(function($) {
-//
-//
-//
-// });
+$('document').ready(function($) {
+
+
+
+
 $(window).scroll(function() {
   if($(this).scrollTop() > 50)  /*height in pixels when the navbar becomes non opaque*/
   {
@@ -12,4 +12,30 @@ $(window).scroll(function() {
       $('#opaque-navbar').removeClass('opaque');
       $('[id="asbestos"]').removeClass('white');
   }
+  });
+
+  // Smooth scrolling
+    var scroll = function(key, val) {
+    	$(key).click(function() {
+        $('body, html').stop().animate({
+          scrollTop: $(val).offset().top
+        }, 1750);
+        return false;
+      });
+    };
+
+    $(function() {
+    	var scrollers = {
+    		'.big-brand': 'body',
+  			'#homie': 'body',
+  			'#aboutie': '#aboutMe',
+  			'#porty': '#myPortfolio',
+  			'#conty': '#contactMe',
+  			'.back-to-top': 'body',
+  			'.fa-envelope-o': '#Contact'
+    	};
+    	for (var key in scrollers) {
+    		scroll(key, scrollers[key]);
+    	}
+    });
 });
