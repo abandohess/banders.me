108c3f1bae3653a54d811d508482d3f76ccff393
abandohess <bandersman@gmail.com>
2016-12-08 23:40:08 -0500

    navbar

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
-$('document').ready(function($) {
-  
-
-
+// $('document').ready(function($) {
+//
+//
+//
+// });
+$(window).scroll(function() {
+  if($(this).scrollTop() > 50)  /*height in pixels when the navbar becomes non opaque*/
+  {
+      $('#opaque-navbar').addClass('opaque');
+      $('[id="asbestos"]').addClass('white');
+  } else {
+      $('#opaque-navbar').removeClass('opaque');
+      $('[id="asbestos"]').removeClass('white');
+  }
 });
