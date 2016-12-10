a20f384d94a0c384ef21e9d0100efcd2a6aac55d
abandohess <bandersman@gmail.com>
2016-12-09 21:16:57 -0500

    menu drawer

--- js/main.js
+++ js/main.js
@@ -1,41 +1,62 @@
 $('document').ready(function($) {
 
-
-
-
-$(window).scroll(function() {
-  if($(this).scrollTop() > 50)  /*height in pixels when the navbar becomes non opaque*/
-  {
-      $('#opaque-navbar').addClass('opaque');
-      $('[id="asbestos"]').addClass('white');
-  } else {
-      $('#opaque-navbar').removeClass('opaque');
+  //transparent menu
+  $(window).scroll(function() {
+    if($(this).scrollTop() < 50) { /*height in pixels when the navbar becomes non opaque*/
+      $('#opaque-navbar').removeClass('opaque');  //navbar becomes clear
       $('[id="asbestos"]').removeClass('white');
-  }
+    }
+    else {
+      $('#opaque-navbar').addClass('opaque'); //navbar becomes black
+      $('[id="asbestos"]').addClass('white');
+    }
   });
 
   // Smooth scrolling
-    var scroll = function(key, val) {
-    	$(key).click(function() {
-        $('body, html').stop().animate({
-          scrollTop: $(val).offset().top
-        }, 1750);
-        return false;
-      });
-    };
-
-    $(function() {
-    	var scrollers = {
-    		'.big-brand': 'body',
-  			'#homie': 'body',
-  			'#aboutie': '#aboutMe',
-  			'#porty': '#myPortfolio',
-  			'#conty': '#contactMe',
-  			'.back-to-top': 'body',
-  			'.fa-envelope-o': '#Contact'
-    	};
-    	for (var key in scrollers) {
-    		scroll(key, scrollers[key]);
-    	}
+  var scroll = function(key, val) {
+  	$(key).click(function() {
+      $('body, html').stop().animate({
+        scrollTop: $(val).offset().top
+      }, 1750);
+      return false;
     });
+  };
+
+  $(function() {
+  	var scrollers = {
+  		'.big-brand': 'body',
+			'[id="homie"]': 'body',
+			'[id="aboutie"]': '#aboutMe',
+			'[id="porty"]': '#myPortfolio',
+			'[id="conty"]': '#contactMe',
+			'.back-to-top': 'body',
+			'.fa-envelope-o': '#Contact'
+  	};
+  	for (var key in scrollers) {
+  		scroll(key, scrollers[key]);
+  	}
+  });
+
+  // collapsable mobile menu
+  $( ".cross" ).hide();
+  $( ".menu" ).hide();
+  $( ".hamburger" ).click(function() {
+  $( ".menu" ).slideToggle( "slow", function() {
+  // $( ".hamburger" ).hide();
+  // $( ".cross" ).show();
+  });
+  });
+
+  $( ".hamburgerItem" ).click(function() {
+  $( ".menu" ).slideToggle( "slow", function() {
+  });
+  });
+
+  // $( ".cross" ).click(function() {
+  // $( ".menu" ).slideToggle( "slow", function() {
+  // $( ".cross" ).hide();
+  // $( ".hamburger" ).show();
+  // });
+  // });
+
 });
