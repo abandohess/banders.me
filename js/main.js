cfabcdb4cb9765f70c240601ec1d27f8af36c9a4
abandohess <bandersman@gmail.com>
2016-12-12 00:00:12 -0500

    navbar highlight

--- js/main.js
+++ js/main.js
@@ -5,10 +5,12 @@ $('document').ready(function($) {
     if($(this).scrollTop() < 50) { /*height in pixels when the navbar becomes non opaque*/
       $('#opaque-navbar').removeClass('opaque');  //navbar becomes clear
       $('[id="asbestos"]').removeClass('white');
+      $('.lessWhite').addClass('lesssWhite');
     }
     else {
       $('#opaque-navbar').addClass('opaque'); //navbar becomes black
       $('[id="asbestos"]').addClass('white');
+      $('.lessWhite').removeClass('lesssWhite');
     }
   });
 
@@ -58,5 +60,50 @@ $('document').ready(function($) {
   // $( ".hamburger" ).show();
   // });
   // });
+
+
+  /**
+   * This part handles the highlighting functionality.
+   * We use the scroll functionality again, some array creation and
+   * manipulation, class adding and class removing, and conditional testing
+   */
+  var aChildren = $("nav a").children(); // find the a children of the list items
+  var aArray = []; // create the empty aArray
+  for (var i=0; i < aChildren.length; i++) {
+      var aChild = aChildren[i];
+      var ahref = $(aChild).attr('href');
+      aArray.push(ahref);
+  } // this for loop fills the aArray with attribute href values
+
+  $(window).scroll(function(){
+      var windowPos = $(window).scrollTop(); // get the offset of the window from the top of page
+      var windowHeight = $(window).height(); // get the height of the window
+      var docHeight = $(document).height();
+
+      for (var i=0; i < aArray.length; i++) {
+          var theID = aArray[i];
+          var divPos = $(theID).offset().top-80; // get the offset of the div from the top of page
+          var divHeight = $(theID).height(); // get the height of the div in question
+          if (windowPos >= divPos && windowPos < (divPos + divHeight)) {
+            if (!$("p[href='" + theID + "']").hasClass("lesssWhite")){
+              $("p[href='" + theID + "']").addClass("nav-active");
+            }
+            if ($("p[href='" + theID + "']").hasClass("lesssWhite") && $("p[href='" + theID + "']").hasClass("nav-active")) {
+              $("p[href='" + theID + "']").removeClass("nav-active");
+            }
+          }
+          else {
+              $("p[href='" + theID + "']").removeClass("nav-active");
+          }
+      }
+
+      if(windowPos + windowHeight == docHeight) {
+          if (!$("nav li:last-child a").hasClass("nav-active")) {
+              var navActiveCurrent = $(".nav-active").attr("href");
+              $("a[href='" + navActiveCurrent + "']").removeClass("nav-active");
+              $("nav li:last-child a").addClass("nav-active");
+          }
+      }
+  });
 
 });
