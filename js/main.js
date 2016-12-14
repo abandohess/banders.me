776f0ec08f24a2689b79258e9e6fd42c3fc1ebf4
abandohess <bandersman@gmail.com>
2016-12-14 02:04:03 -0500

    fixed parallax

--- js/main.js
+++ js/main.js
@@ -1,5 +1,66 @@
 $('document').ready(function($) {
 
+  ;(function ($) {
+
+      $.fn.parallax = function () {
+        var window_width = $(window).width();
+        // Parallax Scripts
+        return this.each(function(i) {
+          var $this = $(this);
+          $this.addClass('parallax');
+
+          function updateParallax(initial) {
+            var container_height;
+            if (window_width < 601) {
+              container_height = ($this.height() > 0) ? $this.height() : $this.children("img").height();
+            }
+            else {
+              container_height = ($this.height() > 0) ? $this.height() : 500;
+            }
+            var $img = $this.children("img").first();
+            var img_height = $img.height();
+            var parallax_dist = img_height - container_height;
+            var bottom = $this.offset().top + container_height;
+            var top = $this.offset().top;
+            var scrollTop = $(window).scrollTop();
+            var windowHeight = window.innerHeight;
+            var windowBottom = scrollTop + windowHeight;
+            var percentScrolled = (windowBottom - top) / (container_height + windowHeight);
+            var parallax = Math.round((parallax_dist * percentScrolled));
+
+            if (initial) {
+              $img.css('display', 'block');
+            }
+            if ((bottom > scrollTop) && (top < (scrollTop + windowHeight))) {
+              $img.css('transform', "translate3D(-50%," + parallax + "px, 0)");
+            }
+
+          }
+
+          // Wait for image load
+          $this.children("img").one("load", function() {
+            updateParallax(true);
+          }).each(function() {
+            if(this.complete) $(this).load();
+          });
+
+          $(window).scroll(function() {
+            window_width = $(window).width();
+            updateParallax(false);
+          });
+
+          $(window).resize(function() {
+            window_width = $(window).width();
+            updateParallax(false);
+          });
+
+        });
+
+      };
+  }( jQuery ));
+
+  $('.parallax').parallax();
+
   //Transparent Menu
   $(window).scroll(function() {
     if($(this).scrollTop() < 50) { /*height in pixels when the navbar becomes non opaque*/
@@ -96,6 +157,15 @@ $('document').ready(function($) {
           }
       }
   });
+
+  //validate contact form has been filled out properly
+  function validateForm() {
+    var x = document.forms["myForm"]["name"].value;
+    if (x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+  }
 
 
 });
