30ad896f124599a9a2e685e037700095af37adac
Andy Bando-Hess <anders.bando-hess.19@dartmouth.edu>
2017-07-16 02:08:22 -0400

    new webpages

---
+++ js/verticalMenu.js
@@ -0,0 +1,10 @@
+// thanks to https://codepen.io/xspydarex/pen/pAbic
+$(document).ready(function() {
+		$('ul.form li a').click(
+			function(e) {
+				// e.preventDefault(); // prevent the default action
+				// e.stopPropagation; // stop the click from bubbling
+				// $(this).closest('ul').find('.selected').removeClass('selected');
+				// $(this).parent().addClass('selected');
+			});
+	});
