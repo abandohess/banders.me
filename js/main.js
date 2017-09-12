793d19adeaeaa7289e841cc9503e8be7bae6eb08
Andy Bando-Hess <anders.bando-hess.19@dartmouth.edu>
2017-09-12 22:38:45 +0200

    all pieces

--- js/main.js
+++ js/main.js
@@ -23,9 +23,25 @@ function cutImageUp() {
 
     // load one piece onto the page
     var size = imagePieces.size;
-    alert(size);
-    var anImageElement = document.getElementById('newImage');
-    anImageElement.src = imagePieces[5];
+
+    var anImageElement1 = document.getElementById('piece1');
+    anImageElement1.src = imagePieces[0];
+    var anImageElement2 = document.getElementById('piece2');
+    anImageElement2.src = imagePieces[1];
+    var anImageElement3 = document.getElementById('piece3');
+    anImageElement3.src = imagePieces[2];
+    var anImageElement4 = document.getElementById('piece4');
+    anImageElement4.src = imagePieces[3];
+    var anImageElement5 = document.getElementById('piece5');
+    anImageElement5.src = imagePieces[4];
+    var anImageElement6 = document.getElementById('piece6');
+    anImageElement6.src = imagePieces[5];
+    var anImageElement7 = document.getElementById('piece7');
+    anImageElement7.src = imagePieces[6];
+    var anImageElement8 = document.getElementById('piece8');
+    anImageElement8.src = imagePieces[7];
+    var anImageElement9 = document.getElementById('piece9');
+    anImageElement9.src = imagePieces[8];
 }
 
 setTimeout(function(){ alert(width); }, 3000);
