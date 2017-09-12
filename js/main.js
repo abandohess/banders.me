d5b60fa3b09843fdfb7e5377c9b89fb60f28ff2f
Andy Bando-Hess <anders.bando-hess.19@dartmouth.edu>
2017-09-12 22:26:42 +0200

    puzzle image

--- js/main.js
+++ js/main.js
@@ -1,102 +1,32 @@
-$('document').ready(function($) {
-  var resizeWidth = 768;
-  // Navar shrinks when user scrolls down 100 pixels
-  $('#mainNav').affix({
-    offset: {
-      top: 100
-    }
-  });
+// var image = new Image();
+// image.src = '../img/amsterReflection.jpg';
 
-  // size of slideshow interface depends on size of image
-  var img = document.getElementById('slidePic');
-  var imgHeight = img.clientHeight;
-  var window_height = $(window).height();
-  if ( imgHeight > window_height) {
-    $(".item").css({"height": window_height});
-  }
-  else {
-    $(".item").css({"height": imgHeight});
-  }
-  // load all slide images into array
-  slidePicsArray = $('.slidePic');
-  $(window).resize(function() {
-    // find the image being displayed
-    for (i = 0; i < slidePicsArray.length; i++) {
-      img = slidePicsArray[i];
-      imgHeight = img.clientHeight;
-      // if not 0, image is being displayed
-      if (imgHeight != 0) {
-        break;
-      }
-    }
-    window_height = $(window).height();
-    if ( imgHeight > window_height) {
-      $(".item").css({"height": window_height});
-    }
-    else {
-      $(".item").css({"height": imgHeight});
-    }
-  });
+var img = document.getElementById('working');
+img.onload = cutImageUp;
+var width = img.clientWidth;
+var height = img.clientHeight;
 
-  // If screen is in mobile mode, make sure links appear in proper format
-  var window_width = $(window).width();
-  if( window_width < resizeWidth ) {
-    document.getElementById('navbarRight').style.float = 'none';
+function cutImageUp() {
+    var imagePieces = [];
+    for(var x = 0; x < 3; ++x) {
+        for(var y = 0; y < 3; ++y) {
+            var canvas = document.createElement('canvas');
+            canvas.width = width/3;
+            canvas.height = height/3;
+            var context = canvas.getContext('2d');
+            context.drawImage(img, x * width/3, y * height/3, width/3, height/3, 0, 0, canvas.width, canvas.height);
+            imagePieces.push(canvas.toDataURL());
+        }
+    }
 
-  }
-  else {
-    document.getElementById('navbarRight').style.float = 'right';
-  }
-  $(window).resize(function() {
-    window_width = $(window).width();
-    if (window_width < resizeWidth) {
-      document.getElementById('navbarRight').style.float = 'none';
-    }
-    else {
-      document.getElementById('navbarRight').style.float = 'right';
-    }
-  });
+    // imagePieces now contains data urls of all the pieces of the image
 
-  // Prevent navbar formatting from getting squished
-  $(window).on("resize", function () {
-    var startResizingNav = 1220;
-    window_width = $(window).width();
-    if (window_width < startResizingNav && window_width > resizeWidth) {
-      //
-      if (window_width > 1000) $(".navLinkFont").css({"margin-left": "-3px"});
+    // load one piece onto the page
+    var size = imagePieces.size;
+    alert(size);
+    var anImageElement = document.getElementById('newImage');
+    anImageElement.src = imagePieces[0];
+}
 
-      if (window_width < 1000) {
-        $(".navLinkFont").css({"margin-left": "-4px"})
-        $(".logoFontRed").css({"font-size": 1.7 + "em"});
-        $(".logoFontBlue").css({"font-size": 1.7 + "em"});
-        $(".navLinkFont").css({"font-size": 1 + "em"});
-        document.getElementById('logo').style.width = '60px';
-      }
-      else {
-        $(".navLinkFont").css({"margin-left": "-3px"})
-        $(".logoFontRed").css({"font-size": 2 + "em"});
-        $(".logoFontBlue").css({"font-size": 2 + "em"});
-        $(".navLinkFont").css({"font-size": 1.5 + "em"});
-        document.getElementById('logo').style.width = '95px';
-      }
-    }
-    else if (window_width < 400) {
-      var nav = document.getElementById('mainNav');
-      var navHeight = nav.clientHeight;
-      $("#myCarousel").css({"margin-top": navHeight})
-      $(".logoFontRed").css({"font-size": 1.7 + "em"});
-      $(".logoFontBlue").css({"font-size": 1.7 + "em"});
-      $(".navLinkFont").css({"font-size": 1.3 + "em"});
-      document.getElementById('logo').style.width = '75px';
-    }
-    else {
-      $(".navLinkFont").css({"margin-left": "0px"})
-      $(".logoFontRed").css({"font-size": 2 + "em"});
-      $(".logoFontBlue").css({"font-size": 2 + "em"});
-      $(".navLinkFont").css({"font-size": 1.5 + "em"});
-      document.getElementById('logo').style.width = '95px';
-    }
-  // Invoke the resize event immediately
-  }).resize();
-
-});
+setTimeout(function(){ alert(width); }, 3000);
+setTimeout(cutImageUp, 5000);
